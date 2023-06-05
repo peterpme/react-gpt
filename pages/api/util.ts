a648432502840be062d6c557a2efa975257fd41d
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-06-04 21:12:59 -0500

    upgrades langchain

--- pages/api/util.ts
+++ pages/api/util.ts
@@ -1,49 +1,57 @@
-import { OpenAI } from "langchain/llms";
-import { LLMChain, ChatVectorDBQAChain, loadQAChain } from "langchain/chains";
-import { HNSWLib } from "langchain/vectorstores";
+import path from "path";
+import { OpenAI } from "langchain/llms/openai";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { PromptTemplate } from "langchain/prompts";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
-const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { BufferMemory } from "langchain/memory";
 
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:`);
+const QA_PROMPT =
+  PromptTemplate.fromTemplate(`You're an helpful react, react-native pro. You understand the inner workings of these frameworks and can also clearly and succintly explain questions and how things work. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
-const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant for the open source javascript library: React. The documentation is located at https://beta.reactjs.org. You have a deep understanding of the React framework. You are given the following extracted parts of a long document and a question. Provide an answer with a hyperlink to the documentation or with a code block directly from the documentation. You should only use hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
-If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not about Reactjs, politely inform them that you are tuned to only answer questions about ReactJs.
-Question: {question}
-=========
 {context}
-=========
-Answer in Markdown:`
-);
 
-export const makeChain = (
-  vectorstore: HNSWLib,
-  onTokenStream?: (token: string) => void
-) => {
-  const questionGenerator = new LLMChain({
-    llm: new OpenAI({ temperature: 0 }),
-    prompt: CONDENSE_PROMPT,
-  });
-  const docChain = loadQAChain(
-    new OpenAI({
-      temperature: 0,
-      streaming: Boolean(onTokenStream),
-      callbackManager: {
-        handleNewToken: onTokenStream,
+Question: {question}
+Helpful answer in markdown:`);
+
+export async function makeChain(onTokenStream?: (token: string) => void) {
+  const dir = path.resolve(process.cwd(), "data");
+  console.log("load vectors from hnswlib");
+  const vectorStore = await HNSWLib.load(dir, new OpenAIEmbeddings());
+
+  const model = new OpenAI({
+    temperature: 0.4,
+    streaming: Boolean(onTokenStream),
+    callbacks: [
+      {
+        handleLLMNewToken(token: string) {
+          if (onTokenStream) {
+            onTokenStream(token);
+          }
+        },
       },
-    }),
-    QA_PROMPT
-  );
-
-  return new ChatVectorDBQAChain({
-    vectorstore,
-    combineDocumentsChain: docChain,
-    questionGeneratorChain: questionGenerator,
+    ],
   });
-};
+
+  console.log("assemble chain from llm");
+  return ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorStore.asRetriever(),
+    {
+      inputKey: "question",
+      qaChainOptions: {
+        type: "stuff",
+        prompt: QA_PROMPT,
+      },
+      returnSourceDocuments: true,
+      memory: new BufferMemory({
+        inputKey: "question",
+        outputKey: "text",
+        memoryKey: "chat_history", // Must be set to "chat_history"
+      }),
+    }
+  );
+}
